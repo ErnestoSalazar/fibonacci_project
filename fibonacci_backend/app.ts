f060edcac2d618059bf5b7b26625898cdfb60868
ErnestoSalazar <ernestoalbertosalazar@gmail.com>
2023-01-19 18:59:46 -0700

    feat: updated endpoint structure

--- fibonacci_backend/app.ts
+++ fibonacci_backend/app.ts
@@ -2,17 +2,19 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import fibonacciRouter from './routes/fibonacci';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', fibonacciRouter);
+app.use('/api/fibonacci', fibonacciRouter);
 
 export default app;
